aa91717061d7029efe6819ad34861333c288af8b
YoGalaxyStar <yogalaxystarmain@gmail.com>
2024-10-30 18:52:23 -0400

    fixed

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -9842,7 +9842,7 @@ local function toggleDisabler(state)
         while disablerEnabled and humanoidRootPart do
             local moveDirection = humanoidRootPart.CFrame.LookVector * (maxSpeed / 2)
             humanoidRootPart.CFrame = humanoidRootPart.CFrame + moveDirection
-            wait(10)
+            wait(1)
         end
     end
 end
